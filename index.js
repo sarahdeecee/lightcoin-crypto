6379fb4bc1828cc16dcf77b7af695e2d405ee1a7
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-05 16:28:46 -0400

    updated all functions

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
@@ -13,35 +19,64 @@ class Deposit extends Transaction {
   get value() {
     return this.amount;
   }
+  isAllowed() {
+    if (this.amount >= 0) {
+      console.log(`Success! deposited ${this.amount}`);
+      return true;
+    } else {
+      console.log(`Please deposited a non-negative amount.`);
+      return false;
+    }
+  }
 }
 
 class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
+  isAllowed() {
+    if (this.account.balance - this.amount >= 0) {
+      console.log(`Success! Withdrew ${this.amount}`);
+      return true;
+    } else {
+      console.log("Insufficient funds.");
+      return false;
+    }
+  }
 }
 
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+    	balance += transaction.value;
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 }
-
 
 // DRIVER CODE BELOW
 
 const myAccount = new Account("snow-patrol");
 
+console.log("Withdrawing $50.25 ...");
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99, myAccount);
+console.log("Depositing $120.00 ...");
+t2 = new Deposit(120.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance);
 
-t3 = new Deposit(120.00, myAccount);
+console.log("Withdrawing $9.99 ...");
+t3 = new Withdrawal(9.99, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
 console.log('Balance:', myAccount.balance);
